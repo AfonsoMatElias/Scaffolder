4d9a7da2bc88377be27caaaf5335c04f36ae64be
Afonso Matumona <afonsomatumona@hotmail.com>
2022-07-24 13:53:23 +0100

    fix: linux bug resolved

--- config/Scaffolders.cs
+++ config/Scaffolders.cs
@@ -7,56 +7,57 @@ using System;
 
 namespace Scaffolder
 {
-    public class Scaffolders
-    {
-        public List<DbModels> Models { get; set; }
-        public dynamic Configurations { get; internal set; } = new object();
+	public class Scaffolders
+	{
+		public List<DbModels> Models { get; set; }
+		public dynamic Configurations { get; internal set; } = new object();
 
-        public Scaffolders(string content)
-        {
-            // Building the content
-            this.Configurations = JsonConvert.DeserializeObject<dynamic>(content);
+		public Scaffolders(string content)
+		{
+			// Building the content
+			this.Configurations = JsonConvert.DeserializeObject<dynamic>(content);
+			var dbModelsPath = Shared.ResolvePath(this.Get("Models")?.FirstOrDefault().DbModels);
 
-            // Loading the models
-            this.Models = Directory.GetFiles(this.Get("Models")?.FirstOrDefault().DbModels).Select(s =>
-            {
-                var fileInfo = new FileInfo(s);
-                return new DbModels
-                {
-                    Name = fileInfo.Name.Replace(".cs", ""),
-                    Path = fileInfo.FullName
-                };
-            }).ToList();
-        }
+			// Loading the models
+			this.Models = Directory.GetFiles(dbModelsPath).Select(s =>
+			{
+				var fileInfo = new FileInfo(s);
+				return new DbModels
+				{
+					Name = fileInfo.Name.Replace(".cs", ""),
+					Path = fileInfo.FullName
+				};
+			}).ToList();
+		}
 
-        public List<Configuration> Get(string key)
-        {
-            try
-            {
-                // Creating the object
-                return JsonConvert.DeserializeObject<List<Configuration>>(
-                    // Accessing the data
-                    this.Configurations[key].ToString()
-                );
-            }
-            catch (System.Exception ex)
-            {
-                Logger.Error("Error: Configuration not found.");
-                Logger.Error($"Error Description: { ex.InnerException?.Message ?? ex.Message }");
+		public List<Configuration> Get(string key)
+		{
+			try
+			{
+				// Creating the object
+				return JsonConvert.DeserializeObject<List<Configuration>>(
+					// Accessing the data
+					this.Configurations[key].ToString()
+				);
+			}
+			catch (System.Exception ex)
+			{
+				Logger.Error("Error: Configuration not found.");
+				Logger.Error($"Error Description: {ex.InnerException?.Message ?? ex.Message}");
 
-                return null;
-            }
-        }
+				return null;
+			}
+		}
 
-        public bool ModelExists(string name)
-        {
-            if (this.Models.Any(m => m.Name == name))
-                return true;
+		public bool ModelExists(string name)
+		{
+			if (this.Models.Any(m => m.Name == name))
+				return true;
 
-            Logger.Error("Model not found.");
+			Logger.Error("Model not found.");
 
-            Shared.Pause();
-            return false;
-        }
-    }
+			Shared.Pause();
+			return false;
+		}
+	}
 }
